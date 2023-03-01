fc1ef18ba0e9e2e17184645ab51d030514d044eb
Patryk Karczewski <patrykkarczewski@yahoo.com>
2023-03-01 17:52:20 +0100

    initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Calculator {
+    double add(double a, double b){
+        return a + b;
+    }
+}
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Calculator calculator= new Calculator();
+        System.out.println(calculator.add(10,5));
+    }
+}
